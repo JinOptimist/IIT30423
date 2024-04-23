56a1554a970776048da880d1308997feaae7e3f1
JinOptimist <88jin@mail.ru>
2024-04-23 21:17:18 +0300

    Lesson 1

---
+++ lesson1.js
@@ -0,0 +1,90 @@
+let name = 'Ivan'
+
+let age = 20 + 2 * 3 // 26
+age = age + 1 // 27
+
+let chance = .02
+
+let isAdult = true
+let isAdmin = false
+// && => and
+let isCoolAdmin = isAdult && isAdmin
+// || => or
+let isNewUser = isAdult || isAdmin
+// ! => not
+let isModerator = !isAdmin // true
+let condition4 = isCoolAdmin || isNewUser && !isModerator
+
+let user1 = {
+	name: 'Olga',
+	age: 20,
+	isWoman: true
+}
+
+let user2 = {
+	name: 'Ivan',
+	age: 25,
+	isWoman: false,
+	adress: {
+		street: 'Push',
+		house: 50,
+		flate: 77
+	}
+}
+
+user1.name = 'Lera'
+user2.adress.house
+
+user1.secondName = 'Ivanov'
+
+// old way
+// console.log('Hi ' + user1.name + ' Cool ' + user1.age + ' age')
+
+// new way
+// console.log(`Hi ${user1.name}. Cool ${user1.age} age`)
+
+
+function sayHiToUser(user){
+	console.log(`Hi ${user.name}. It was greate ${user.age} years`)
+}
+
+sayHiToUser(user1)
+sayHiToUser(user2)
+
+let users = []
+
+for(let i = 0; i < 10 ; i++){
+	let user = {
+		name: 'Ivan ' + i,
+		age: 40 - i
+	}
+	
+	users.push(user)
+}
+
+//for( AAA ; BBB ; CCC){
+// DDD	
+//}
+
+// AAA
+// BBB true
+// DDD
+// CCC
+// BBB true
+// DDD
+// CCC
+// BBB false
+
+
+for(let i = 0; i < users.length; i++){
+	let user = users[i]
+	sayHiToUser(user)	
+}
+	
+
+
+
+
+
+
+
---
+++ practice1.js
@@ -0,0 +1,50 @@
+const numbers = [5,4,9,0,6,7,8,2]
+
+// Task 1. Get max number
+let currentMax = numbers[0]
+
+for(let i = 0; i < numbers.length; i++){
+	const current = numbers[i]
+	if (currentMax < current){
+		currentMax = current
+	}
+}
+
+console.log(`Max number ${currentMax}`)
+
+
+// Task 2. Get max pair
+let currentSumm = 0
+let maxSumm = 0
+
+for(let i = 0; i < numbers.length - 1; i++){
+	const current1 = numbers[i]
+	const current2 = numbers[i + 1]
+	currentSumm = current1 + current2
+	if (currentSumm > maxSumm){
+		maxSumm = currentSumm
+	}
+}
+
+console.log(`Max pair sum is ${maxSumm}`)
+
+
+// Task 3. Last odd
+
+let lastOddNumber = 0
+for(let i = 0; i < numbers.length; i++){
+	const number = numbers[i]
+	const isOdd = number % 2 == 1
+	if (isOdd){
+		lastOddNumber = number
+	}
+}
+
+console.log(`Last odd number ${lastOddNumber}`)
+
+// Task 4. Summ first and last number
+const first = array[0]
+const last = array[array.length - 1]
+const summ = first + last
+console.log(summ)
+
