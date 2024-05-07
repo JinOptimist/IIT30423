e06133e8282cc8990feb68207e986ba6a8d95a68
JinOptimist <88jin@mail.ru>
2024-05-07 20:38:35 +0300

    Toggle images

--- js/pair.js
+++ js/pair.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
+
     const cards = document.getElementsByClassName('card');
 
     for (let i = 0; i < cards.length; i++) {
@@ -7,17 +7,41 @@ document.addEventListener("DOMContentLoaded", function () {
         card.addEventListener('click', onCardClick)
     }
 
-    function onCardClick(evt){
+    function onCardClick(evt) {
         const card = evt.currentTarget;
-        
+
+        const isFaceActive = card
+            .querySelector('.face')
+            .classList
+            .contains('active');
+
+        if (isFaceActive) {
+            showCover(card);
+        } else {
+            showFace(card)
+        }
+    }
+
+    function showFace(card) {
         const cover = card.querySelector('.cover');
         cover.style.display = 'none'
+        cover.classList.remove('active');
 
         const face = card.querySelector('.face');
         face.style.display = 'block'
+        face.classList.add('active');
+    }
+
+    function showCover(card) {
+        const cover = card.querySelector('.cover');
+        cover.style.display = 'block'
+        cover.classList.add('active');
+
+        const face = card.querySelector('.face');
+        face.style.display = 'none'
+        face.classList.remove('active');
     }
 });
 
 
 
-  
